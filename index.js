4512bc2afc1ce8058935937af217d679e90aef8c
Colton Egbert <145735468+ctegbert@users.noreply.github.com>
2024-10-19 20:15:03 -0500

    Mongo Update

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ app.use(cors());
 console.log("Mongo URI:", process.env.MONGO_URI);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
+
 
 // Routes setup
 const carRoutes = require('./routes/carRoutes');
