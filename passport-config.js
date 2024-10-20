af6ddce972aa87ed5b4fc01a1a816d185321563b
Colton Egbert <145735468+ctegbert@users.noreply.github.com>
2024-10-19 21:49:01 -0500

    Passport JWT

--- passport-config.js
+++ passport-config.js
@@ -1,30 +1,25 @@
-const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('./models/User');
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
 
 module.exports = (passport) => {
   passport.use(
-    new LocalStrategy(async (username, password, done) => {
+    new JwtStrategy(opts, async (jwt_payload, done) => {
       try {
-        const user = await User.findOne({ username });
-        if (!user) return done(null, false, { message: 'User not found' });
-
-        const isMatch = await user.isValidPassword(password);
-        if (!isMatch) return done(null, false, { message: 'Incorrect password' });
-
-        return done(null, user);
+        const user = await User.findById(jwt_payload.id);
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
       } catch (error) {
-        return done(error);
+        return done(error, false);
       }
     })
   );
-
-  passport.serializeUser((user, done) => done(null, user.id));
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch (error) {
-      done(error);
-    }
-  });
 };
